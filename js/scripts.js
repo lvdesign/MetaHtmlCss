af643c55d2973d4afa2ac2085aed6b7ebc0643bc
lvdesignfr <work@lvdesign.com.fr>
2022-08-29 17:37:24 +0200

    base

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+`use strict`
+/* Year */
+var today = new Date();
+let myYearVariable = today.getFullYear();
+console.log(myYear);
+document.getElementById("myYear").textContent = myYearVariable;
---
+++ readme.md
@@ -0,0 +1,7 @@
+# HTML and CSS in depth
+# Meta
+
+## Submit your portfolio project
+
+
+Apply the skills you learned in this course to introduce more advanced styling into your portfolio.
